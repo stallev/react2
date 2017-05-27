d33dedf0a32aacf5ee0b2fdccad2943ad7f23028
stallev <stwork87@gmail.com>
2017-05-27 16:17:55 +0300

    added basket

--- src/Basket.js
+++ src/Basket.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './css/Basket.css';
+import BasketRow from "./BasketRow";
 
 class Basket extends Component {
   constructor(props){
     super(props);
     this.state = {
     
-    }
+    };
+    this.renderBasketRows = this.renderBasketRows.bind(this);
+  }
+  renderBasketRows(){
+    let basketRows = [];
+    let increment = 0;
+    let localGoods = JSON.parse(localStorage.getItem('goods'));
+    localGoods.map((i) => {
+      if(i.count > 0){
+        basketRows.push(
+          <BasketRow
+            key = {increment}
+            goodsTitle = {i.kind}
+            goodsCount = {i.count}
+          />
+        );
+      }
+      increment++;
+    });
+    return basketRows;
   }
   render() {
     return (
@@ -15,11 +35,10 @@ class Basket extends Component {
           Корзина
         </div>
         <div className="basket__row-list">
-          {this.props.basketContent()}
+          {this.renderBasketRows()}
         </div>
       </div>
     );
   }
 }
-
 export default Basket;
